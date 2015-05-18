d3deddd2bd99fba11d32f4eb718afd2060dafda2
love-jerry <502005775@qq.com>
2015-05-18 13:00:31 +0800

    update

---
+++ art-parent/art-web/src/main/java/com/cr/art/controller/IndexController.java
@@ -0,0 +1,21 @@
+package com.cr.art.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping(value="/index")
+	@ResponseBody
+	public Object test() {
+		Map<String,String> result = new HashMap<String,String>();
+		result.put("code", "ok");
+		return result;
+	}
+	
+}
